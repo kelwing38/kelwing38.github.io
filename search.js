d817e6e933724f4b29889e13a80ab9247138199a
kelwing38 <kelvinwangyuhan@gmail.com>
2023-08-05 17:13:22 +0800

    Initial commit

---
+++ search.js
@@ -0,0 +1,11 @@
+// Get a reference to the search button
+const searchButton = document.getElementById("search-btn");
+
+// Add a click event listener to the search button
+searchButton.addEventListener("click", function(event) {
+
+    // Get the value entered in the search box
+    const searchInput = document.getElementById("search-box").value;
+
+    fetch()
+});
